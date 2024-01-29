932af1c170196d3fb4f85ee280ca8afe957d50dd
unknown <reihanazifirdaus12@gmail.com>
2024-01-29 13:44:43 +0700

    variabel object

---
+++ object.js
@@ -0,0 +1,6 @@
+var orang = new Object();
+orang.nama = "Reihan Azi Firdaus";
+orang.alamat = "jalan jenderal sudirman gg.mukalmi rt.02 rw.03/86";
+orang.noHP = "083100592886";
+alert(orang.nama);
+console.log(orang);
