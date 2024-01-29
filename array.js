eaf99c1b4c78e25052a050cd0fabe2799e88ed6a
unknown <reihanazifirdaus12@gmail.com>
2024-01-29 13:35:25 +0700

    variabel array

---
+++ array.js
@@ -0,0 +1,8 @@
+// var namaarray = new array()
+var lemari = new Array("dompet", 5000 , true);
+console.log(lemari[1]);
+console.log(lemari[2]);
+
+lemari[3] = "ktp";
+lemari.push("ATM");
+console.log(lemari);
--- fungsi.js
+++ fungsi.js
@@ -11,4 +11,9 @@ function jumlah(a,b){
     return a + b;
 }
 
+var kurang = function (a,b){
+    return a - b;
+}
+
 alert(jumlah(5,20));
+alert(kurang(25,5));
