94d31a1e1bbc25b348e9435539dc2e87cd4ad56c
unknown <reihanazifirdaus12@gmail.com>
2024-01-30 11:41:08 +0700

    dom 1

--- javascript-dom/object.js
+++ javascript-dom/object.js
@@ -4,5 +4,17 @@ siswa.namaBelakang ="Azi Firdaus"
 siswa.namaLengkap = function(){
     return this.namaDepan + "" +this.namaBelakang;
 };
+
+//object json
+var siswa2 = {
+    namaDepan: "raid ",
+    namaBelakang: "sadad",
+    namaLengkap: function(){
+        return this.namaDepan + "" + this.namaBelakang;
+    },
+};
+
+
 alert("nama: " + siswa.namaLengkap());
+alert("nama: " + siswa2.namaLengkap());
 
